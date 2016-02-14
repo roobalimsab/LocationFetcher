2861c04d7d57ebf5bc3f6484b1978a15a03501a6
roobab <roobab@INroobab.local>
2016-02-14 21:20:07 +0530

    Rooba | Added server call for fetching the current location

--- app/src/main/java/com/example/roobab/locationfetcher/MainActivity.java
+++ app/src/main/java/com/example/roobab/locationfetcher/MainActivity.java
@@ -16,6 +16,11 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 import static android.net.ConnectivityManager.CONNECTIVITY_ACTION;
 import static android.net.wifi.WifiManager.RSSI_CHANGED_ACTION;
 import static android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION;
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView locationNameView;
     private RadioGroup choices;
     private static final int MSG_FETCH_WIFI_STRENGTH = 1;
+    private SignalServer signalServer;
 
     Handler H = new Handler() {
         @Override
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signalServer = getSignalServer();
         setUpViews();
     }
 
@@ -88,6 +95,18 @@ public class MainActivity extends AppCompatActivity {
     private void readSignalStrength() {
         WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
         List<ScanResult> aps = wifiManager.getScanResults();
+        signalServer.fetchCurrentLocation(new TypedJsonString("{\"aps\": \"" + aps + "\"}"), new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                System.out.println("Fetch Success: " + response);
+                locationNameView.setText(response.toString());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                System.out.println("Fetch Error: " + error);
+            }
+        });
     }
 
     private void unregisterReceivers() {
@@ -99,12 +118,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void recordCurrentWifiSignals() {
-
+        H.sendEmptyMessage(MSG_FETCH_WIFI_STRENGTH);
     }
 
     private void setupWifiSignalReceivers() {
         registerReceiver(wifiReceiver, new IntentFilter(CONNECTIVITY_ACTION));
         registerReceiver(wifiReceiver, new IntentFilter(SCAN_RESULTS_AVAILABLE_ACTION));
         registerReceiver(wifiReceiver, new IntentFilter(RSSI_CHANGED_ACTION));
+    }
+
+    private SignalServer getSignalServer() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://192.168.0.30:9090")
+                .build();
+        return restAdapter.create(SignalServer.class);
     }
 }
---
+++ app/src/main/java/com/example/roobab/locationfetcher/SignalServer.java
@@ -0,0 +1,15 @@
+package com.example.roobab.locationfetcher;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+public interface SignalServer {
+
+    @GET("/api/fetchCurrentLocation")
+    public void fetchCurrentLocation(@Query("currentSignal") TypedJsonString signalJson, Callback<Response> cb);
+
+}
